355c3d4d64d319eced62de031a66dcab61b01638
Preeti Gusain <preetigusain@Preetis-MacBook-Air.local>
2025-02-09 16:58:56 +0530

    Intializing Project

---
+++ lib/screens/splash_screen.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({super.key});
+
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        decoration: const BoxDecoration(
+          color: Colors.orangeAccent,
+        ),
+        child: const Center(
+          child: Text(
+            'Phone Thrift',
+            style: TextStyle(
+              fontSize: 34,
+              fontWeight: FontWeight.w700,
+              color: Colors.white,
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
